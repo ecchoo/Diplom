beb2d874d654190f7a069c28b98379b55fe24d15
Vadya <vadya.695@gmail.com>
2024-04-03 14:13:15 +0300

    server initialization

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json());
+
+app.options('*', (req, res) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.sendStatus(200);
+});
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running on port ${port}`);
+});
